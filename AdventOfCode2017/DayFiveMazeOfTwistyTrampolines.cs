9fa077ec07b429e8afebfef79fc401efd2d10af7
Raubin Kumar <raubinkumar@gmail.com>
2017-12-08 08:06:10 +0530

    Day 5 - Maze Of Twisty Trampolines

---
+++ AdventOfCode2017/DayFiveMazeOfTwistyTrampolines.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2017.Helpers;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class DayFiveMazeOfTwistyTrampolines
+    {
+        /// <summary>
+        /// Gets the number of steps to come out from maze.
+        /// </summary>
+        /// <param name="steps">The steps.</param>
+        /// <returns>Step count.</returns>
+        public static int GetNumberOfStepsToComeOutFromMaze(int[] steps)
+        {
+            int stepCount = 0;
+            int currentPosition = 0;
+
+            while (currentPosition < steps.Count())
+            {
+                int previousLocation = currentPosition;
+
+                currentPosition += steps[currentPosition];
+
+                steps[previousLocation] = steps[previousLocation] + 1;
+
+                stepCount++;
+            }
+
+            return stepCount;
+        }
+
+        /// <summary>
+        /// Gets the number of steps to come out from maze.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>step count.</returns>
+        public static int GetNumberOfStepsToComeOutFromMaze(string fileName)
+        {
+            string path = ReadFromTextFile.GetFilePath(fileName);
+
+            string[] dataString = ReadFromTextFile.GetAllLines(path);
+            int[] data = dataString.Select(int.Parse).ToArray();
+
+            return GetNumberOfStepsToComeOutFromMaze(data);
+        }
+    }
+}
---
+++ AdventOfCode2017Tests/DayFiveMazeOfTwistyTrampolinesTest.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2017Tests
+{
+    [TestClass]
+    public class DayFiveMazeOfTwistyTrampolinesTest
+    {
+        [TestMethod]
+        public void DayFiveTestMethod1()
+        {
+            int[] steps = new int[6] { 0, 3, 0, 1, -3, 1 };
+            int count = DayFiveMazeOfTwistyTrampolines.GetNumberOfStepsToComeOutFromMaze(steps);
+            Assert.AreEqual(6, count);
+        }
+
+        [TestMethod]
+        public void DayFiveTestMethod2()
+        {
+            int count = DayFiveMazeOfTwistyTrampolines.GetNumberOfStepsToComeOutFromMaze("Day5Data.txt");
+            Assert.AreEqual(375042, count);
+        }
+    }
+}
